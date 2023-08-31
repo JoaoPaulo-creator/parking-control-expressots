b023e20ae382391fc5db8b6e853dade2e0e74254
JoaoPaulo-creator <joaosarto@gmail.com>
2023-08-30 23:39:17 -0300

    refactor: fixed the case from dto file name

--- src/useCases/parkingSpot/parking-spot.module.ts
+++ src/useCases/parkingSpot/parking-spot.module.ts
@@ -1,7 +1,7 @@
 import { ContainerModule } from "inversify";
 import { CreateModule } from "@expressots/core";
 import { CreateParkingSpotController } from "./create/parking-spot-create.controller";
-import { FindAllParkingSpotController } from "./findAll/parking-spot-findall.controller";
+import { FindAllParkingSpotController } from "./findall/parking-spot-findall.controller";
 import { FindOneParkingSpotController } from "./findOne/findone-parking-spot.controller";
 
 const ParkingSpotModule: ContainerModule = CreateModule([
