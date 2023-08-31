e338886d0b5f308c99d65c6b95b4d50d59b66239
JoaoPaulo-creator <joaosarto@gmail.com>
2023-08-30 23:38:42 -0300

    refactor: returning an ordered mapped object

--- src/useCases/parkingSpot/findall/parking-spot-findall.usecase.ts
+++ src/useCases/parkingSpot/findall/parking-spot-findall.usecase.ts
@@ -1,6 +1,6 @@
 import { ParkingSpotRepository } from "@repositories/parkingspot/parking-spot.repository";
 import { provide } from "inversify-binding-decorators";
-import { IFindAllParkingSpotsResponseDTO } from "./parking-spot-findAll.dto";
+import { IFindAllParkingSpotsResponseDTO } from "./parking-spot-findall.dto";
 
 @provide(FindAllParkingSpotsUseCase)
 class FindAllParkingSpotsUseCase {
@@ -8,7 +8,22 @@ class FindAllParkingSpotsUseCase {
 
   async execute(): Promise<IFindAllParkingSpotsResponseDTO[] | null> {
     const spots = await this.parkingRepository.findAll();
-    return spots;
+    const mappedSpots: IFindAllParkingSpotsResponseDTO[] = [];
+
+    spots.forEach((s) => {
+      mappedSpots.push({
+        id: s.id,
+        apartment: s.apartment,
+        block: s.block,
+        brandCar: s.block,
+        modelCar: s.modelCar,
+        colorCar: s.colorCar,
+        licensePlate: s.licensePlate,
+        responsibleName: s.responsibleName,
+      });
+    });
+
+    return mappedSpots;
   }
 }
 
