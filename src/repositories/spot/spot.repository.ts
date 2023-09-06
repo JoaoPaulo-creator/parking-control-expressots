99d32d3a60b384abe39b8890ca74aa268262dc8a
JoaoPaulo-creator <joaosarto@gmail.com>
2023-09-05 23:14:21 -0300

    refactor: removed unused imports

--- src/repositories/spot/spot.repository.ts
+++ src/repositories/spot/spot.repository.ts
@@ -1,6 +1,4 @@
-import { ParkingSpot } from '@entities/parking-spot.entity';
 import { Spot } from '@entities/spot.entity';
-import { TypeORMProvider } from '@providers/orm/typeorm/typeorm.provider';
 import { BaseRepository } from '@repositories/base-repository';
 import { provide } from 'inversify-binding-decorators';
 
